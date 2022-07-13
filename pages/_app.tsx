0d09cb1550a9caf2fcbe686e75e68a6684d0013f
Uzair Saiyed <uzairhajra76330@gmail.com>
2022-07-13 18:35:45 +0530

    Bug Fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Children } from 'react';
 import Head from 'next/head';
 import '../styles/globals.css'
 import "../styles/toggleSwitch.css"
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar/Navbar';
 import { ThemeProvider } from 'next-themes';
-import { AnimatePresence } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
+import { useRouter } from 'next/router';
 
-function MyApp({ Component, pageProps, router, isVisible }) {
+function MyApp({ Component, pageProps }) {
   const [toggled, setToggled] = React.useState(false);
   const handleClick = () => {
     setToggled((s) => !s);
   };
   // const name = document.querySelector('.resume-container');
+  const router = useRouter();
   return <ThemeProvider attribute='class'>
     <Head>
       <title>Portfolio | Uzair Saiyed</title>
@@ -21,9 +23,24 @@ function MyApp({ Component, pageProps, router, isVisible }) {
       <div className="col-span-12 p-4 text-center transition duration-75 ease-in-out bg-white lg:col-span-3 rounded-2xl dark:bg-dark-500 shadow-custom-light dark:shadow-custom-dark"><Sidebar toggled={toggled} handleClick={handleClick} /></div>
       <div className="flex flex-col col-span-12 overflow-hidden bg-white lg:col-span-9 rounded-2xl dark:bg-dark-500 shadow-custom-light dark:shadow-custom-dark">
         <Navbar />
-        <AnimatePresence exitBeforeEnter>
-          isVisible && <Component {...pageProps} key={"modal"} />
-        </AnimatePresence>
+        {/* <AnimatePresence exitBeforeEnter>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence> */}
+        <AnimatePresence exitBeforeEnter />
+        <motion.div initial={{ opacity: 0 }} animate={{
+          opacity: 1, transition: {
+            delay: 0.25,
+            duration: 0.5
+          }
+        }} exit={{
+          opacity: 0, transition: {
+            delay: 0.25,
+            ease: 'easeInOut'
+          }
+        }} key={router.route} className="content">
+          <Component {...pageProps} />
+        </motion.div>
+        {/* </AnimatePresence> */}
       </div>
     </div>
   </ThemeProvider>
