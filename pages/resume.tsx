1555faaf3ee264ca3f8039c0710ab46b1d36e7a1
Uzair Saiyed <uzairhajra76330@gmail.com>
2022-08-25 20:58:05 +0530

    Updated Profile

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -12,30 +12,61 @@ const resume = () => {
     return (
         <motion.div variants={routeAnimation} initial="initial" animate="animate" exit="exit" className='px-6 py-2 overflow-x-hidden overflow-y-scroll resume-container' style={{ maxHeight: "68.5vh" }}>
             {/* Education and Experience */}
-            <div className="grid gap-6 md:grid-cols-2">
+            <div className="grid gap-6 md:grid-cols-2 mx-5">
                 <motion.div variants={fadeInUp} initial="initial" animate="animate">
                     <h5 className='my-3 text-2xl font-bold'>Education</h5>
+                    <hr />
+                    <hr />
                     <div className=''>
-                        <div className="education">
-                            <h5 className='my-2 text-xl font-bold'>B.E. Computer Science Engineering</h5>
-                            <p className='font-semibold'>Sarvajanik College of Engineering and Technology{"(2021-2024)"}</p>
-                            <p className='my-3'>CPI: 8.7{"(Till Sem-3)"}</p>
-                        </div>
-                        <div className="education">
-                            <h5 className='my-2 text-xl font-bold'>Diploma in Computer Engineering</h5>
-                            <p className='font-semibold'>Tapi Diploma Engineering College{"(2018-2021)"}</p>
-                            <p className='my-3'>CPI: 9.8</p>
-                        </div>
+                        <ul style={{listStyleType:'circle'}}>
+                            <li>
+                                <div className="education">
+                                    <h5 className='my-2 text-xl font-bold'>B.E. Computer Science Engineering</h5>
+                                    <p className='font-semibold'>Sarvajanik College of Engineering and Technology{"(2021-2024)"}</p>
+                                    <p className='my-3'><i><b>CPI: 8.7</b></i>{"(Till Sem-3)"}</p>
+                                </div>
+                            </li>
+                            <hr />
+                            <li>
+                                <div className="education">
+                                    <h5 className='my-2 text-xl font-bold'>Diploma in Computer Engineering</h5>
+                                    <p className='font-semibold'>Tapi Diploma Engineering College{"(2018-2021)"}</p>
+                                    <p className='my-3'><i><b>CPI: 9.8</b></i></p>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </motion.div>
                 <motion.div variants={fadeInUp} initial="initial" animate="animate">
                     <h5 className='my-3 text-2xl font-bold'>Experience</h5>
+                    <hr />
+                    <hr />
                     <div>
-                        <div className="education">
-                            <h5 className='my-2 text-xl font-bold'>Website Development</h5>
-                            <p className='font-semibold'>Personal Projects</p>
-                            <p className='my-3'>Self taught developer. I have been building websites for more than 2 years now!</p>
-                        </div>
+                        <ul style={{listStyleType:'circle'}}>
+                            <li>
+                            <div className="education">
+                                <h5 className='my-2 text-xl font-bold'>Website Development</h5>
+                                <p className='font-semibold'>Personal Projects</p>
+                                <p className='my-3'>Self taught developer. I have been building websites for more than 2 years now!</p>
+                            </div>
+                            </li>
+                            <hr />
+                            <li>
+                                <div className="internship">
+                                    <h5 className='my-2 text-xl font-bold'>GeeksForGeeks </h5>
+                                    <p className='font-semibold'>Technical Content Writer Intern</p>
+                                    <p className='my-3 text-sky-500'><a href="https://auth.geeksforgeeks.org/user/uzairhajra76330/articles"><i>View My Published work here!</i></a></p>
+                                </div>
+                            </li>
+                            <hr />
+                            <li>
+                                <div className="">
+                                    <h5 className='my-2 text-xl font-bold'>Google Developer Student Club</h5>
+                                    <p className='font-semibold'>GDSC Web Development Lead</p>
+                                    <p className='my-3 text-sky-500'><a href="g.dev/JustUzair"><i>View Profile Here!</i></a></p>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </motion.div>
             </div>
